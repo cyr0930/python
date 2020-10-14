7a02355336435326ec6786f4f8dd73dfa9f33f12
youngrok cha <line0930@gmail.com>
2020-10-15 08:59:40 +0900

    remove validation

--- machine_learning/nlp/model.py
+++ machine_learning/nlp/model.py
@@ -1,36 +1,37 @@
+import math
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 INIT_STD = 0.02
 
 
 class Block(nn.Module):
-    def __init__(self, embed_dim, heads, dropout):
+    def __init__(self, embed_dim, nheads, nlayers, dropout):
         super(Block, self).__init__()
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
         self.ln_1 = nn.LayerNorm(embed_dim)
         self.ln_2 = nn.LayerNorm(embed_dim)
-        self.attn = nn.MultiheadAttention(embed_dim, heads)
-        self.linear1 = nn.Linear(embed_dim, embed_dim * 4)
-        self.linear2 = nn.Linear(embed_dim * 4, embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, nheads)
+        self.linear1 = nn.Linear(embed_dim, embed_dim * 4, bias=False)
+        self.linear2 = nn.Linear(embed_dim * 4, embed_dim, bias=False)
         self.mlp = nn.Sequential(self.linear1, nn.GELU(), self.linear2)
 
-        self._init_weights()
+        self._init_weights(nlayers)
 
-    def _init_weights(self):
+    def _init_weights(self, nlayers):
+        scaled_std = INIT_STD / math.sqrt(2 * nlayers)
         self.attn.in_proj_weight.data.normal_(std=INIT_STD)
+        self.attn.out_proj.weight.data.normal_(std=scaled_std)
         self.linear1.weight.data.normal_(std=INIT_STD)
-        self.linear2.weight.data.normal_(std=INIT_STD)
+        self.linear2.weight.data.normal_(std=scaled_std)
         self.attn.in_proj_bias.data.zero_()
-        self.linear1.bias.data.zero_()
-        self.linear2.bias.data.zero_()
+        self.attn.out_proj.bias = None
 
     def forward(self, x):
-        attn_mask = torch.full(
-            (len(x), len(x)), -float("Inf"), device=x.device, dtype=x.dtype
-        )
+        attn_mask = torch.full((len(x), len(x)), -float("Inf"), device=x.device, dtype=x.dtype)
         attn_mask = torch.triu(attn_mask, diagonal=1)
 
         x = self.ln_1(x)
@@ -42,8 +43,7 @@ class Block(nn.Module):
 
 
 class GPT2(pl.LightningModule):
-    def __init__(self, embed_dim: int, heads: int, layers: int, num_positions: int, vocab_size: int,
-                 dropout: float = 0.1):
+    def __init__(self, embed_dim, nheads, nlayers, num_positions, vocab_size, dropout=0.1):
         super(GPT2, self).__init__()
         self.save_hyperparameters()
 
@@ -51,8 +51,8 @@ class GPT2(pl.LightningModule):
         self.position_embeddings = nn.Embedding(num_positions, embed_dim)
         self.dropout = nn.Dropout(dropout)
         self.layers = nn.ModuleList()
-        for _ in range(layers):
-            self.layers.append(Block(embed_dim, heads, dropout))
+        for _ in range(nlayers):
+            self.layers.append(Block(embed_dim, nheads, nlayers, dropout))
         self.ln_f = nn.LayerNorm(self.hparams.embed_dim)
         self.head = nn.Linear(embed_dim, vocab_size, bias=False)
 
@@ -76,4 +76,6 @@ class GPT2(pl.LightningModule):
             h = layer(h)
         h = self.ln_f(h)
 
-        return self.head(h)
+        h = self.head(h)
+        # h = F.linear(h, self.token_embeddings.weight)  # weight tying
+        return h
--- machine_learning/nlp/train.py
+++ machine_learning/nlp/train.py
@@ -8,11 +8,32 @@ from torch.utils.tensorboard import SummaryWriter
 from machine_learning.nlp.model import GPT2
 from machine_learning.nlp.tokenizer import get_tokenizer
 from machine_learning.nlp.preprocess import PATH_DATA
+from transformers.optimization import get_cosine_schedule_with_warmup
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pad_id = 1
 eos_id = 2
 bptt = 64
 batch_size = 16
+
+
+def batchify(data, bsz):
+    data = torch.tensor([data]).t()
+    nbatch = data.size(0) // bsz
+    data = data.narrow(0, 0, nbatch * bsz)
+    data = data.view(bsz, -1).t()
+    data = data.contiguous()
+    return data.to(device)
+
+
+def get_batch(source, i):
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].view(-1)
+    mask = target > -1
+    return data, target, mask
+
+
 tokenizer = get_tokenizer()
 data = []
 with open(PATH_DATA, encoding='utf-8') as r:
@@ -28,48 +49,22 @@ with open(PATH_DATA, encoding='utf-8') as r:
         if j % 10000 == 0:
             print(j)
 random.shuffle(data)
-data = [y for x in data for y in x]
-train_txt = data[:int(len(data) * 0.99)]
-val_txt = data[len(train_txt):]
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def batchify(data, bsz):
-    data = torch.tensor([data]).t()
-    nbatch = data.size(0) // bsz
-    data = data.narrow(0, 0, nbatch * bsz)
-    data = data.view(bsz, -1).t()
-    data = data.contiguous()
-    return data.to(device)
-
-
+train_txt = [y for x in data for y in x]
 train_data = batchify(train_txt, batch_size)
-val_data = batchify(val_txt, batch_size)
-
-
-def get_batch(source, i):
-    seq_len = min(bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    mask = target > -1
-    return data, target, mask
-
 
 ntokens = len(tokenizer)
 emsize = 768
 nlayers = 12
 nhead = 12
 dropout = 0.1
+lr = 5e-5
 epochs = 100
 nbatches = len(train_data) // bptt
-writer = SummaryWriter()
 
 model = GPT2(emsize, nhead, nlayers, bptt, ntokens, dropout).to(device)
-
 criterion = nn.CrossEntropyLoss(reduction='none', ignore_index=pad_id)
-lr = 0.00005
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.95)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999), eps=1e-8)
+scheduler = get_cosine_schedule_with_warmup(optimizer, 2000, epochs * nbatches)
 
 
 def train():
@@ -83,12 +78,13 @@ def train():
             output = model(data)
             loss = criterion(output.view(-1, ntokens), targets)
         loss.mean().backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
+        scheduler.step()
 
         total_loss += (loss * mask).sum().item()
         total_mask += mask.sum().item()
-        log_interval = 50
+        log_interval = 100
         if batch % log_interval == 0 and batch > 0:
             cur_loss = total_loss / total_mask
             elapsed = time.time() - start_time
@@ -101,27 +97,7 @@ def train():
             start_time = time.time()
 
 
-def evaluate(eval_model, data_source):
-    eval_model.eval()
-    total_loss = 0.
-    with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, bptt):
-            data, targets, mask = get_batch(data_source, i)
-            output = eval_model(data)
-            output_flat = output.view(-1, ntokens)
-            loss = criterion(output_flat, targets)
-            loss = ((loss * mask).sum() / mask.sum())
-            total_loss += len(data) * loss.item()
-    return total_loss / (len(data_source) - 1)
-
-
+writer = SummaryWriter()
 for epoch in range(1, epochs + 1):
-    epoch_start_time = time.time()
     train()
-    val_loss = evaluate(model, val_data)
-    print('-' * 89)
-    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | valid ppl {:8.2f}'.format(
-        epoch, (time.time() - epoch_start_time), val_loss, math.exp(val_loss)))
-    print('-' * 89)
-    # scheduler.step()
 writer.close()
